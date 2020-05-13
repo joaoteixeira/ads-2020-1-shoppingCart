c203fe299f49ad0a262d56511f7a28c59432d3bc
Wígny <wignybora@gmail.com>
2020-05-12 21:13:49 -0300

    Add models

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -142,7 +142,7 @@
   <script src="{{ asset('js/bootstrap.bundle.min.js') }}"> </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
   <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script> -->
-  <!-- <script src="{{ asset('js/dashboard.js') }}"> </script> -->
+  <script src="{{ asset('js/dashboard.js') }}"> </script>
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  $usuarios = \App\Usuario::all();
-  return $usuarios;
-  // view('welcome')
-});
+Route::get('/', fn () => view('welcome'));
 
 Route::get('/sobre', fn () => view('sobre'));
